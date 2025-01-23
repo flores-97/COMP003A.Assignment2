939368c915511876045d1deef7e3c07a8979f5c9
flores-97 <floresvictor15@gmail.com>
2025-01-22 18:41:40 -0800

    created calculations for student and senior discounts

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -8,7 +8,48 @@ namespace COMP003A.Assignment2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userFname;
+            string UserLname;
+            int userAge;
+            double productPrice;
+            bool isStudent;
+            
+
+            Console.WriteLine("What is your first name?");
+            userFname = Console.ReadLine();
+            
+            Console.WriteLine("What is your last name?");
+            UserLname = Console.ReadLine();
+            //these two will read the first and last name of user. 
+
+            Console.WriteLine("What is your age?");
+            userAge = int.Parse(Console.ReadLine());
+            //This will read user's age
+
+            Console.WriteLine("What is the price of the item you're interested in?");
+            productPrice = int.Parse(Console.ReadLine());
+            //this will read the price of an said item
+
+            Console.WriteLine("Are you a student? (True/False)");
+            isStudent = bool.Parse(Console.ReadLine());
+            //will determine if user is student
+
+            //following lines will perform calculations
+            int futureAge = userAge + 5;
+            bool isSenior = userAge >= 60;
+            bool isDiscount = isSenior && futureAge >= 60;
+            double isStudent_discount = productPrice * 0.9;
+            double isSenior_discount = productPrice * 0.8;
+
+
+            //following lines will be output
+            Console.WriteLine($"\nHello {userFname} {UserLname}!");
+            Console.WriteLine($"Your current age is {userAge}.");
+            Console.WriteLine($"In 5 years, you will be {futureAge}.");
+            Console.WriteLine($"The origanl price of the item is ${productPrice}.");
+            Console.WriteLine($"As a student, your discounted price is ${isStudent_discount}.");
+            Console.WriteLine($"As a senior citizen, your discounted price would be ${isSenior_discount}.");
+
         }
     }
 }
