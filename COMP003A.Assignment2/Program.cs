5a2bfcf439139faec8ccf072ca147516db8f3c35
flores-97 <floresvictor15@gmail.com>
2025-01-22 18:56:31 -0800

    enter calculator title

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -13,8 +13,9 @@ namespace COMP003A.Assignment2
             int userAge;
             double productPrice;
             bool isStudent;
-            
-
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("Welcome to the Discount Calculator!");
+            Console.WriteLine("*****************************************************");
             Console.WriteLine("What is your first name?");
             userFname = Console.ReadLine();
             
@@ -33,15 +34,14 @@ namespace COMP003A.Assignment2
             Console.WriteLine("Are you a student? (True/False)");
             isStudent = bool.Parse(Console.ReadLine());
             //will determine if user is student
-
+            
             //following lines will perform calculations
             int futureAge = userAge + 5;
-            bool isSenior = userAge >= 60;
-            bool isDiscount = isSenior && futureAge >= 60;
+            bool isSenior = userAge >= 60;        
             double isStudent_discount = productPrice * 0.9;
             double isSenior_discount = productPrice * 0.8;
 
-
+            Console.WriteLine("*****************************************************");
             //following lines will be output
             Console.WriteLine($"\nHello {userFname} {UserLname}!");
             Console.WriteLine($"Your current age is {userAge}.");
@@ -49,7 +49,7 @@ namespace COMP003A.Assignment2
             Console.WriteLine($"The origanl price of the item is ${productPrice}.");
             Console.WriteLine($"As a student, your discounted price is ${isStudent_discount}.");
             Console.WriteLine($"As a senior citizen, your discounted price would be ${isSenior_discount}.");
-
+            Console.WriteLine("*****************************************************");
         }
     }
 }
