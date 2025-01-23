8bb1a0ebff7c24203679947dac24f3c2d020cbd4
flores-97 <floresvictor15@gmail.com>
2025-01-22 16:53:36 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*AUthor: Victor Flores
+ Course: COMP003A
+Faculty: Jonathon Cruz
+Purpose: A program demonstrating a discount calculator.*/
+namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
